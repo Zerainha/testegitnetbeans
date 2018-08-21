672eeb98a7dc34f7a1909410d2ee0488c2d1f7af
brenoneto <brenoneto@brenoneto-HP-Pavilion-dv2600>
2018-08-21 09:53:55 -0300

    PRIMERA COMMIT COM NETBEANS!

---
+++ src/gitzinho/testegitnetbeans.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitzinho;
+
+/**
+ *
+ * @author brenoneto
+ */
+public class testegitnetbeans {
+    public static void main(String[] args) {
+        System.out.println("PRIMEIRO ARQUIVO COM NETBEANS E GIT");
+    }
+    
+}
